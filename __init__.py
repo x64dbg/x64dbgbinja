5e6f9f4bb8d094c74e6614a7acbde8aa77f35c06
Malware Utkonos <utkonos@malwarolo.gy>
2024-01-13 22:45:54 -0500

    Update to Python3, refresh, migrate from deprecated API components

--- __init__.py
+++ __init__.py
@@ -1,106 +1,102 @@
-from binaryninja import *
-import os, sqlite3, traceback
+"""Binary Ninja plugin to import and export symbols and comments to x64dbg database format."""
+import json
+import pathlib
+
+from binaryninja.enums import SymbolType
+from binaryninja.interaction import get_save_filename_input, get_open_filename_input
+from binaryninja.plugin import PluginCommand
+from binaryninja.settings import Settings
+from binaryninja.types import Symbol
+
+s = Settings()
+s.register_group('dd', 'x64dbg Database Export')
+setting = {
+    'description': 'Always export comments to the x64dbg database.',
+    'title': 'Export Comments',
+    'default': True,
+    'type': 'boolean'
+}
+s.register_setting('dd.comments', json.dumps(setting))
 
 
-def get_module_name(view):
-    filename = view.file.filename
-    if filename.endswith(".bndb"):
-        try:
-            conn = sqlite3.connect(filename)
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM global WHERE name='filename'")
-            _, rawfilename = cursor.fetchone()
-            filename = rawfilename[5:-2]
-        except:
-            pass
-    return os.path.basename(filename)
+def export_db(bv):
+    """Export symbols and optionally comments from Binary Ninja to an x64dbg database."""
+    db = dict()
+    module = pathlib.Path(bv.file.original_filename)
+    dbext = 'dd{}'.format(bv.arch.default_int_size * 8)
 
-
-def export_db(view):
-    db = {}
-    module = get_module_name(view).lower()
-    base = view.start
-    dbext = "dd%d" % (view.arch.default_int_size * 8)
-
-    file = get_save_filename_input("Export database", "*.%s" % dbext, "%s.%s" %
-                                   (module, dbext))
-    if not file:
+    if not (f := get_save_filename_input('Export database', dbext, f'{module.stem}.{dbext}')):
         return
-    print "Exporting database %s" % file
+    file = pathlib.Path(f)
+    print(f'Exporting database: {file}')
 
-    print "Exporting symbols"
-    db["labels"] = [{
-        "text": symbol.name,
-        "manual": False,
-        "module": module,
-        "address": "0x%X" % (symbol.address - base)
-    } for symbol in view.get_symbols()]
-    print "%d label(s) exported" % len(db["labels"])
+    print('Exporting symbols')
+    db['labels'] = [
+        {
+            'text': symbol.name,
+            'manual': not symbol.auto,
+            'module': module.name.lower(),
+            'address': '0x{:X}'.format(symbol.address - bv.start)
+        }
+        for symbol in bv.get_symbols()
+    ]
+    print('Label(s) exported: {}'.format(len(db['labels'])))
 
-    db["comments"] = [{
-        "text": func.comments[comment].replace("{", "{{").replace("}", "}}"),
-        "manual": False,
-        "module": module,
-        "address": "0x%X" % (comment - base)
-    } for func in view.functions for comment in func.comments]
-    print "%d comment(s) exported" % len(db["comments"])
+    s = Settings()
+    if s.get_bool('dd.comments'):
+        db['comments'] = [
+            {
+                'text': func.comments[address].replace('{', '{{').replace('}', '}}'),
+                'manual': True,
+                'module': module.name.lower(),
+                'address': '0x{:X}'.format(address - bv.start)
+            }
+            for func in bv.functions for address in func.comments
+        ]
+        print('Comment(s) exported: {}'.format(len(db['comments'])))
 
-    with open(file, "w") as outfile:
-        json.dump(db, outfile, indent=1)
-    print "Done!"
+    file.write_text(json.dumps(db))
+    print('Done!')
 
 
-def import_db(view):
-    db = {}
-    module = get_module_name(view).lower()
-    base = view.start
+def import_db(bv):
+    """Import x64dbg database to Binary Ninja."""
+    module = pathlib.Path(bv.file.original_filename).name.lower()
 
-    file = get_open_filename_input("Import database", "*.dd%d" %
-                                   (view.arch.default_int_size * 8))
-    if not file:
+    if not (f := get_open_filename_input('Import database', '*.dd{}'.format(bv.arch.default_int_size * 8))):
         return
-    print "Importing database %s" % file
+    file = pathlib.Path(f)
+    print(f'Importing database: {file}')
 
-    with open(file) as dbdata:
-        db = json.load(dbdata)
+    db = json.load(file.open())
 
     count = 0
-    labels = db.get("labels", [])
+    labels = db.get('labels', list())
     for label in labels:
-        try:
-            if label["module"] != module:
+        if label['module'] != module:
+            continue
+        address = int(label['address'], 16) + bv.start
+        name = label['text']
+        if symbol := bv.get_symbol_at(address):
+            if symbol.name == name:
                 continue
-            address = int(label["address"], 16) + base
-            name = label["text"]
-            symbol = view.get_symbol_at(address)
-            if not symbol or symbol.name != name:
-                view.define_user_symbol(Symbol(FunctionSymbol, address, name))
-                count += 1
-        except:
-            traceback.print_exc()
-            pass
-    print "%d/%d label(s) imported" % (count, len(labels))
+        bv.define_user_symbol(Symbol(SymbolType.FunctionSymbol, address, name))
+        count += 1
+    print('Label(s) imported: {}/{}'.format(count, len(labels)))
 
     count = 0
-    comments = db.get("comments", [])
+    comments = db.get('comments', list())
     for comment in comments:
-        try:
-            if comment["module"] != module:
-                continue
-            address = int(comment["address"], 16) + base
-            comment = comment["text"]
-            for func in view.functions:
-                func.set_comment(address, comment)
-            count += 1
-        except:
-            traceback.print_exc()
-            pass
-    print "%d/%d comment(s) imported" % (count, len(comments))
+        if comment['module'] != module:
+            continue
+        address = int(comment['address'], 16) + bv.start
+        for func in bv.get_functions_containing(address):
+            func.set_comment_at(address, comment['text'])
+        count += 1
+    print('Comment(s) imported: {}/{}'.format(count, len(comments)))
 
-    print "Done!"
+    print('Done!')
 
 
-PluginCommand.register("Export x64dbg database", "Export x64dbg database",
-                       export_db)
-PluginCommand.register("Import x64dbg database", "Import x64dbg database",
-                       import_db)
+PluginCommand.register('Export x64dbg database', 'Export x64dbg database', export_db)
+PluginCommand.register('Import x64dbg database', 'Import x64dbg database', import_db)
